edc2b8df2419e622c91090b3ad8e173b65cb4973
bowuor <benowuor88@gmail.com>
2016-10-16 14:51:12 +0300

    string utils server url

--- app/src/main/java/com/example/wladek/pocketcard/util/StringUtils.java
+++ app/src/main/java/com/example/wladek/pocketcard/util/StringUtils.java
@@ -4,5 +4,5 @@ package com.example.wladek.pocketcard.util;
  * Created by wladek on 9/12/16.
  */
 public abstract class StringUtils {
-    public static final String SERVER_URL = "http://pocke.cloudapp.net/api";
+    public static final String SERVER_URL = "http://pockecard.cloudapp.net/api";
 }
